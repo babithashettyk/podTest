3c19cba13d7b372ccbc15d85f566d2b9044fcf98
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-09 10:48:53 +0530

    pods added

---
+++ podTest/podTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  podTest
+//
+//  Created by Babitha Shetty K on 09/11/21.
+//
+
+import Cocoa
+import Alamofire
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
